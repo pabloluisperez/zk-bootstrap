fe18fe3c3ddf4adc39899d0f914c4f7ca0d50f9b
Pablo <pabloluisperez@gmail.com>
2014-03-13 09:42:28 +0100

    Bootstrap 3.1.1

---
+++ src/main/java/org/teralco/addons/bootstrap/Version.java
@@ -0,0 +1,7 @@
+package org.teralco.addons.bootstrap;
+
+public class Version {
+	/** Returns the version UID.
+	 */
+	public static final String UID = "1.0.1";
+}
--- src/test/java/test/ctrl/DemoWindowComposer.java
+++ src/test/java/test/ctrl/DemoWindowComposer.java
@@ -1,12 +1,11 @@
 package test.ctrl;
 
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.event.ForwardEvent;
-import org.zkoss.zk.ui.event.MouseEvent;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 
 public class DemoWindowComposer extends GenericForwardComposer {
 	
+	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 		
